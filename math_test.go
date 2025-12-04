5042f5836a5ea16a912223cc33dc28064e82ddbb
MiguelFrog <miguelcardozo@frogsummit.com>
2025-12-04 20:39:04 -0300

    feat: :sparkles: Ajustando soma

--- math_test.go
+++ math_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	total := Soma(15, 15)
-	if total != 30 {
+	total := Soma(5, 5)
+	if total != 10 {
 		t.Errorf("Resultado da soma é invalido: Resultado %d, Esperado: %d", total, 30)
 	}
 }
